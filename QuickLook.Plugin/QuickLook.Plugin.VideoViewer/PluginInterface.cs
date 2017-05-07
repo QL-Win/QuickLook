719d2772bf12c760e4318d6fe804d0ab1f610df7
Paddy Xu <xupaddy@gmail.com>
2017-05-08 00:57:03 +0300

    A simple VideoViewer

---
+++ QuickLook.Plugin/QuickLook.Plugin.VideoViewer/PluginInterface.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using Unosquare.FFmpegMediaElement;
+
+namespace QuickLook.Plugin.VideoViewer
+{
+    public class PluginInterface : IViewer
+    {
+        private ViewerPanel _vp;
+
+        public int Priority => int.MaxValue;
+
+        public bool CanHandle(string path)
+        {
+            if (Directory.Exists(path))
+                return false;
+
+            var formats = new[]
+            {
+                ".3g2", ".3gp", ".3gp2", ".3gpp", ".amv", ".asf", ".asf", ".avi", ".flv", ".m2ts", ".m4v", ".mkv",
+                ".mov", ".mp4", ".mp4v", ".mpeg", ".mpg", ".ogv", ".qt", ".vob", ".webm", ".wmv"
+            };
+
+            if (formats.Contains(Path.GetExtension(path).ToLower()))
+                return true;
+
+            return false;
+        }
+
+        public void Prepare(string path, ContextObject context)
+        {
+            using (var element = new MediaElement {Source = new Uri(path)})
+            {
+                context.SetPreferredSizeFit(new Size(element.NaturalVideoWidth, element.NaturalVideoHeight), 0.6);
+            }
+        }
+
+        public void View(string path, ContextObject context)
+        {
+            _vp = new ViewerPanel();
+
+            context.ViewerContent = _vp;
+
+            _vp.LoadAndPlay(path);
+
+            context.Title = $"{Path.GetFileName(path)} ({TimeSpan.FromSeconds(_vp.mediaElement.NaturalDuration).ToString(@"hh\:mm\:ss")}, {_vp.mediaElement.NaturalVideoWidth} × {_vp.mediaElement.NaturalVideoHeight} )";
+            context.IsBusy = false;
+        }
+
+        public void Dispose()
+        {
+            _vp?.Dispose();
+        }
+    }
+}
---
+++ QuickLook.Plugin/QuickLook.Plugin.VideoViewer/ViewerPanel.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuickLook.Plugin.VideoViewer
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class ViewerPanel : UserControl, IDisposable
+    {
+        public ViewerPanel()
+        {
+            InitializeComponent();
+        }
+
+        public void LoadAndPlay(string path)
+        {
+            mediaElement.Source = new Uri(path);
+            mediaElement.Play();
+        }
+
+        ~ViewerPanel()
+        {
+            GC.SuppressFinalize(this);
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            mediaElement?.Dispose();
+        }
+    }
+}
