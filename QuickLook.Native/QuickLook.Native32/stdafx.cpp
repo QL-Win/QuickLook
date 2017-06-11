be8f31a8c89badd58da1f8c1fb715c73b30a2a4d
Paddy Xu <xupaddy@gmail.com>
2017-06-11 20:20:52 +0300

    add GPL file header

--- QuickLook.Native/QuickLook.Native32/stdafx.cpp
+++ QuickLook.Native/QuickLook.Native32/stdafx.cpp
@@ -1,6 +1,19 @@
-// stdafx.cpp : source file that includes just the standard includes
-// QuickLook.Shell32Helper.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
+// Copyright © 2017 Paddy Xu
+// 
+// This file is part of QuickLook program.
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "stdafx.h"
 
--- QuickLook.Native/QuickLook.Native32/stdafx.h
+++ QuickLook.Native/QuickLook.Native32/stdafx.h
@@ -1,7 +1,19 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
-//
+// Copyright © 2017 Paddy Xu
+// 
+// This file is part of QuickLook program.
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
