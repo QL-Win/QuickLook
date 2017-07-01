3e9f9eeec9b998146ec0d2b3d25af4b165555ad7
Paddy Xu <xupaddy@gmail.com>
2017-07-01 23:27:41 +0300

    Add csv viewer

---
+++ QuickLook.Plugin/QuickLook.Plugin.CsvViewer/CsvViewerPanel.xaml.cs
@@ -0,0 +1,101 @@
+﻿// Copyright © 2017 Paddy Xu
+// 
+// This file is part of QuickLook program.
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using CsvHelper;
+
+namespace QuickLook.Plugin.CsvViewer
+{
+    /// <summary>
+    ///     Interaction logic for CsvViewerPanel.xaml
+    /// </summary>
+    public partial class CsvViewerPanel : UserControl
+    {
+        public CsvViewerPanel()
+        {
+            InitializeComponent();
+        }
+
+        public List<string[]> Rows { get; private set; } = new List<string[]>();
+
+        public void LoadFile(string path)
+        {
+            const int limit = 100000;
+            var binded = false;
+
+            using (var sr = new StreamReader(path))
+            {
+                using (var parser = new CsvParser(sr))
+                {
+                    var i = 0;
+                    while (true)
+                    {
+                        var row = parser.Read();
+                        if (row == null)
+                            break;
+                        row = Concat(new[] {$"{i++ + 1}".PadLeft(6)}, row);
+
+                        if (!binded)
+                        {
+                            SetupColumnBinding(row.Length);
+                            binded = true;
+                        }
+
+                        if (i > limit)
+                        {
+                            Rows.Add(Enumerable.Repeat("...", row.Length).ToArray());
+                            break;
+                        }
+
+                        Rows.Add(row);
+                    }
+                }
+            }
+        }
+
+        private void SetupColumnBinding(int rowLength)
+        {
+            for (var i = 0; i < rowLength; i++)
+            {
+                var col = new DataGridTextColumn
+                {
+                    FontFamily = new FontFamily("Consolas"),
+                    FontWeight = FontWeight.FromOpenTypeWeight(i == 0 ? 700 : 400),
+                    Binding = new Binding($"[{i}]")
+                };
+                dataGrid.Columns.Add(col);
+            }
+        }
+
+        public static T[] Concat<T>(T[] x, T[] y)
+        {
+            if (x == null) throw new ArgumentNullException("x");
+            if (y == null) throw new ArgumentNullException("y");
+            var oldLen = x.Length;
+            Array.Resize(ref x, x.Length + y.Length);
+            Array.Copy(y, 0, x, oldLen, y.Length);
+            return x;
+        }
+    }
+}
---
+++ QuickLook.Plugin/QuickLook.Plugin.CsvViewer/Plugin.cs
@@ -0,0 +1,71 @@
+// Copyright © 2017 Paddy Xu
+// 
+// This file is part of QuickLook program.
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.IO;
+using System.Windows;
+
+namespace QuickLook.Plugin.CsvViewer
+{
+    public class Plugin : IViewer
+    {
+        private CsvViewerPanel _panel;
+
+        public int Priority => int.MaxValue;
+        public bool AllowsTransparency => true;
+
+        public void Init()
+        {
+        }
+
+        public bool CanHandle(string path)
+        {
+            if (Directory.Exists(path))
+                return false;
+
+            return Path.GetExtension(path).ToLower() == ".csv";
+        }
+
+        public void Prepare(string path, ContextObject context)
+        {
+            context.PreferredSize = new Size {Width = 1000, Height = 700};
+        }
+
+        public void View(string path, ContextObject context)
+        {
+            _panel = new CsvViewerPanel();
+
+            context.ViewerContent = _panel;
+            context.Title = $"{Path.GetFileName(path)}";
+            _panel.LoadFile(path);
+
+            context.IsBusy = false;
+        }
+
+        public void Cleanup()
+        {
+            GC.SuppressFinalize(this);
+
+            _panel = null;
+        }
+
+        ~Plugin()
+        {
+            Cleanup();
+        }
+    }
+}
