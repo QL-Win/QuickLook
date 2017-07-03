c5c478cc1c79565fa7cd85a28df9bfae7fff5178
Paddy Xu <xupaddy@gmail.com>
2017-07-04 00:01:28 +0300

    remove old build pkgs

--- Scripts/rename-pkg.ps1
+++ Scripts/rename-pkg.ps1
@@ -1,4 +1,7 @@
 $version = git describe --always --tags
 
+Remove-Item ..\Build\QuickLook-$version.msi -ErrorAction SilentlyContinue
+Remove-Item ..\Build\QuickLook-$version.zip -ErrorAction SilentlyContinue
+
 Rename-Item ..\Build\QuickLook.msi QuickLook-$version.msi
 Compress-Archive ..\Build\Package\* ..\Build\QuickLook-$version.zip
