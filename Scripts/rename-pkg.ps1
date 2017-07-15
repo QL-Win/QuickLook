0c021217b15f261c6251a425c243c9c6f3642bc5
Paddy Xu <xupaddy@gmail.com>
2017-07-15 15:20:50 +0300

    deal with FileInUse exception when packing .zip

--- Scripts/rename-pkg.ps1
+++ Scripts/rename-pkg.ps1
@@ -3,5 +3,7 @@ $version = git describe --always --tags
 Remove-Item ..\Build\QuickLook-$version.msi -ErrorAction SilentlyContinue
 Remove-Item ..\Build\QuickLook-$version.zip -ErrorAction SilentlyContinue
 
+Start-Sleep -s 1
+
 Rename-Item ..\Build\QuickLook.msi QuickLook-$version.msi
 Compress-Archive ..\Build\Package\* ..\Build\QuickLook-$version.zip
