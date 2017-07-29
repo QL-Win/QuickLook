694713ba17bae198e03641ef7b707bc705c2597f
Paddy Xu <xupaddy@gmail.com>
2017-07-29 22:28:48 +0300

    set limit to button size

---
+++ QuickLook/Converters/ScaledValueConverter.cs
@@ -0,0 +1,44 @@
+﻿// Copyright © 2017 Paddy Xu
+// 
+// This file is part of QuickLook program.
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace QuickLook.Converters
+{
+    public class ScaledValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double scalingFactor = 0;
+            if (parameter != null)
+                double.TryParse((string) parameter, out scalingFactor);
+
+            if (Math.Abs(scalingFactor) < 0.0001)
+                return double.NaN;
+
+            return (double) value * scalingFactor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            CultureInfo culture)
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+    }
+}
