5a83dce8ed6833658e9c23c5f4aa2887bb1ea11c
Paddy Xu <xupaddy@gmail.com>
2017-11-09 20:22:42 +0200

    sometime this throws

--- QuickLook/ViewerWindow.xaml.cs
+++ QuickLook/ViewerWindow.xaml.cs
@@ -156,7 +156,8 @@ namespace QuickLook
             else
             {
                 _restoreForDragMove = WindowState == WindowState.Maximized;
-                DragMove();
+                if (e.LeftButton == MouseButtonState.Pressed)
+                    DragMove();
             }
         }
     }
