197bfee1da5a1c099a69d110849a4cd4f3d9d598
Paddy Xu <xupaddy@gmail.com>
2018-01-27 16:42:56 +0200

    fix path

--- QuickLook.Common/Properties/AssemblyInfo.cs
+++ QuickLook.Common/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("QuickLook.Plugin.IViewerPlugin")]
+[assembly: AssemblyTitle("QuickLook.Common")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("QuickLook.Plugin.IViewerPlugin")]
+[assembly: AssemblyProduct("QuickLook.Common")]
 [assembly: AssemblyCopyright("Copyright © Paddy Xu 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
