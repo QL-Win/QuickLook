031f667caacff0492dfdaa77c0ba6708fba21e56
Paddy Xu <xupaddy@gmail.com>
2018-07-28 20:38:43 +0300

    remove useless signing scripts

--- Scripts/sign-msi.ps1
+++
@@ -1,8 +0,0 @@
-if(-not (Test-Path env:CI))
-{
-    $signExe = 'C:\Program Files (x86)\Windows Kits\10\bin\10.0.16299.0\x86\signtool.exe'
-    $file = '..\Build\QuickLook.msi'
-    $timestampUrl = 'http://time.certum.pl/'
-
-    .$signExe sign /a /v /fd sha256 /t $timestampUrl $file
-}
