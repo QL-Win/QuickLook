1f13aa66788b1dfb5c1d983ba8eeaf6f4e360790
ema <mccoy39082@163.com>
2025-07-01 04:30:08 +0800

    Fix PDF thumbnail rendering crash #1531

--- QuickLook.Plugin/QuickLook.Plugin.PDFViewer/AsyncPageToThumbnailConverter.cs
+++ QuickLook.Plugin/QuickLook.Plugin.PDFViewer/AsyncPageToThumbnailConverter.cs
@@ -18,6 +18,7 @@
 using System;
 using System.Globalization;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Media.Imaging;
 
@@ -30,7 +31,7 @@ internal class AsyncPageToThumbnailConverter : IMultiValueConverter
 
     public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
     {
-        if (values.Length < 2)
+        if (values.Length < 3)
             throw new Exception("PageIdToImageConverter");
 
         if (values[0] is not PdfDocumentWrapper handle) return null;
@@ -38,11 +39,17 @@ internal class AsyncPageToThumbnailConverter : IMultiValueConverter
         var pageId = (int)values[1];
         if (pageId < 0) return null;
 
+        if (values[2] is not UserControl thisPdfViewer) return null;
+
         var task = Task.Run(() =>
         {
             try
             {
-                return handle.RenderThumbnail(pageId);
+                if (handle is null || thisPdfViewer is null) return Loading;
+                return thisPdfViewer.Dispatcher.Invoke(() =>
+                {
+                    return handle.RenderThumbnail(pageId);
+                });
             }
             catch (Exception)
             {
