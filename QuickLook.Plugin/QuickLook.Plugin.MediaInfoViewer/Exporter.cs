b0dceffa9481b1dd87d321e9e16416e107221bfb
ema <mccoy39082@163.com>
2025-08-05 00:58:48 +0800

    Add Exporter class for MediaInfo integration

---
+++ QuickLook.Plugin/QuickLook.Plugin.MediaInfoViewer/Exporter.cs
@@ -0,0 +1,16 @@
+﻿using MediaInfoLib;
+using System.ComponentModel.Composition;
+
+namespace QuickLook.Plugin.MediaInfoViewer;
+
+[Export]
+public static class Exporter
+{
+    public static MediaInfo Open(string path)
+    {
+        MediaInfo lib = new MediaInfo()
+           .WithOpen(path);
+
+        return lib;
+    }
+}
