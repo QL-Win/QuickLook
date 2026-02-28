14c4b6f440a3be4d8931405c04600e272c509954
Aarav Maloo <aaravmaloo06@gmail.com>
2026-02-28 13:57:55 +0530

    Update Plugin.cs
    the previous and this commit basically fixed the issues reported by sourceery-ai on the pr

--- QuickLook.Plugin/QuickLook.Plugin.Hdf5Viewer/Plugin.cs
+++ QuickLook.Plugin/QuickLook.Plugin.Hdf5Viewer/Plugin.cs
@@ -2,6 +2,7 @@ using QuickLook.Common.Plugin;
 using System;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace QuickLook.Plugin.Hdf5Viewer;
@@ -10,6 +11,7 @@ public sealed class Plugin : IViewer
 {
     private static readonly byte[] Hdf5Signature = { 0x89, 0x48, 0x44, 0x46, 0x0D, 0x0A, 0x1A, 0x0A };
     private static readonly string[] SupportedExtensions = { ".h5", ".hdf5", ".hdf", ".he5" };
+    private static readonly long[] SignatureProbeOffsets = { 0, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288 };
     private Hdf5TextPanel _panel;
 
     public int Priority => 0;
@@ -40,37 +42,38 @@ public sealed class Plugin : IViewer
         _panel = new Hdf5TextPanel();
         context.ViewerContent = _panel;
         context.Title = Path.GetFileName(path);
+        context.IsBusy = true;
 
-        try
+        var panel = _panel;
+
+        Task.Run(() =>
         {
-            _panel.SetText(Hdf5SummaryBuilder.Build(path));
-        }
-        catch (Exception ex)
+            try
+            {
+                return Hdf5SummaryBuilder.Build(path);
+            }
+            catch (Exception ex)
+            {
+                return
+                    $"Failed to open HDF5 file.{Environment.NewLine}{Environment.NewLine}" +
+                    $"{ex.GetType().Name}: {ex.Message}";
+            }
+        }).ContinueWith(t =>
         {
-            _panel.SetText(
-                $"Failed to open HDF5 file.{Environment.NewLine}{Environment.NewLine}" +
-                $"{ex.GetType().Name}: {ex.Message}");
-        }
-        finally
-        {
+            if (panel is not null)
+                panel.SetText(t.Result);
+
             context.IsBusy = false;
-        }
+        }, TaskScheduler.FromCurrentSynchronizationContext());
     }
 
     public void Cleanup()
     {
-        GC.SuppressFinalize(this);
         _panel = null;
     }
 
     private static bool HasHdf5Signature(string path)
     {
-        var probes = new long[12];
-        probes[0] = 0;
-
-        for (var i = 1; i < probes.Length; i++)
-            probes[i] = 512L << (i - 1);
-
         try
         {
             using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
@@ -80,7 +83,7 @@ public sealed class Plugin : IViewer
 
                 var header = new byte[Hdf5Signature.Length];
 
-                foreach (var offset in probes)
+                foreach (var offset in SignatureProbeOffsets)
                 {
                     if (offset + Hdf5Signature.Length > stream.Length)
                         break;
